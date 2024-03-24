ead3813e1cb5a26a158853b960e1e2d4445fcb3f
Maxim Drumov <Drumov.M.A@gmail.com>
2024-03-24 21:28:25 +0300

    Add method in Subs repository
    Signed-off-by: Maxim Drumov <Drumov.M.A@gmail.com>

--- src/main/java/com/nexign/bootcamp/emulator/repository/SubscriberRepository.java
+++ src/main/java/com/nexign/bootcamp/emulator/repository/SubscriberRepository.java
@@ -3,5 +3,9 @@ package com.nexign.bootcamp.emulator.repository;
 import com.nexign.bootcamp.emulator.model.Subscriber;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Optional;
+
 public interface SubscriberRepository extends ListCrudRepository<Subscriber, Long> {
+
+    Optional<Subscriber> findByPhoneNumber (Long phoneNumber);
 }
