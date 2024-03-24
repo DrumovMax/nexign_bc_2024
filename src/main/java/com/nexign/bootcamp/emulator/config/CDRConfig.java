4e80b80001c2fdc0d75840d6d6591cf552784a1a
Maxim Drumov <Drumov.M.A@gmail.com>
2024-03-24 21:26:17 +0300

    Change pom and properties
    Signed-off-by: Maxim Drumov <Drumov.M.A@gmail.com>

--- src/main/java/com/nexign/bootcamp/emulator/config/CDRConfig.java
+++ src/main/java/com/nexign/bootcamp/emulator/config/CDRConfig.java
@@ -1,15 +1,26 @@
 package com.nexign.bootcamp.emulator.config;
 
+import com.nexign.bootcamp.emulator.commands.ShellCommands;
 import com.nexign.bootcamp.emulator.component.InitBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CDRConfig {
+public class DatabaseConfig {
 
-    @Bean(initMethod = "initCDRFiles")
-    public InitBean initCDRFiles () {
+    @Bean(initMethod = "initSubs")
+    public InitBean initSubscriber () {
         return new InitBean();
     }
 
+    @Bean(initMethod = "startEmulate")
+    public InitBean emulateSwitchWork () { return new InitBean(); }
+
+    @Bean(initMethod = "subs")
+    public ShellCommands initMessage () {
+        System.out.println("Call help, to explore commands");
+        return new ShellCommands();
+    }
+
 }
+
